5f1056b7fa1b8a6f578f67e7b53e7c3d095ee2d5
totomus83 <tom.dibellonio@gmail.com>
2025-01-13 15:50:04 +0100

    test

--- python-hello-world/2-print.py
+++ python-hello-world/2-print.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def greet():
-    print("\"Programming is like building a multilingual puzzle")
+    print("\"Programming is like building a multilingual puzzle\n")
 
 
 greet()
