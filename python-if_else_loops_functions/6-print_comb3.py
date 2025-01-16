cc06caa1564d5d7be9c17b8bf4b186d5b8fd2f8e
totomus83 <tom.dibellonio@gmail.com>
2025-01-16 16:25:56 +0100

    test

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ number2 = 0
 for number1 in range(10):
     for number2 in range(10):
         if number1 == 8 and number2 == 9:
-            print(f"{number1}{number2}", end="")
+            print("{:01}{:01}".format(number1, number2), end="")
         elif number1 < number2 and number1 != 8 and number2 != 9:
-            print(f"{number1}{number2}", end=", ")
+            print("{:01}{:01}".format(number1, number2), end=", ")
