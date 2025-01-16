38de3158fb05f5c6c093c1a35d1b4f3b083fcbe7
totomus83 <tom.dibellonio@gmail.com>
2025-01-16 16:30:54 +0100

    fixed issue with the printing of number 79 due to elif

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 number1 = 0
-number2 = 0
+number2 = 1
 for number1 in range(10):
-    for number2 in range(10):
+    for number2 in range(number1 + 1, 10):
         if number1 == 8 and number2 == 9:
             print("{:01}{:01}".format(number1, number2), end="")
-        elif number1 < number2 and number1 != 8 and number2 != 9:
+        else:
             print("{:01}{:01}".format(number1, number2), end=", ")
