fe8c6f55acbe4980a1748f355029031ac36180d5
totomus <tom.dibellonio@gmail.com>
2025-01-17 20:10:31 +0100

    fixed pycodestyle error

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -7,4 +7,3 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, calculator.sub(a, b)))
     print("{} * {} = {}".format(a, b, calculator.mul(a, b)))
     print("{} / {} = {}".format(a, b, calculator.div(a, b)))
-    
