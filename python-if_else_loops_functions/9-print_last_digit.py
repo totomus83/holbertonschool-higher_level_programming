b23e129883b3a82fe491291786e86473ba06c1df
totomus <tom.dibellonio@gmail.com>
2025-01-17 18:08:05 +0100

    test

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def print_last_digit(number):
     if number >= 0:
-        last_digit = number % 10
+        last_digit = abs(number) % 10
     else:
-        last_digit = 10 - number % 10
+        last_digit = 10 - abs(number) % 10
     print(last_digit, end='')
     return (last_digit)
