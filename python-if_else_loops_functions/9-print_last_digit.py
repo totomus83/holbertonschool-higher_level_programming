0483a3c75d6e3df2ca0d9fa4b8b94d7f696ad24c
totomus <tom.dibellonio@gmail.com>
2025-01-17 17:50:37 +0100

    removes parenthessis

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     if number >= 0:
         last_int = number % 10
     else:
-        last_int = (10 - number % 10)
+        last_int = 10 - number % 10
     print(last_int, end='')
     return (last_int)
