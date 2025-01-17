b4e343154b4023f32b87c0cab5eb5cc9f8a73a12
totomus <tom.dibellonio@gmail.com>
2025-01-17 20:23:54 +0100

    test

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) - 1 == 0:
-        print("{} arguments.".format(len(sys.argv) - 1))
-    elif len(sys.argv) - 1 == 1:
-        print("{} argument:".format(len(sys.argv) - 1))
-    else:
-        print("{} arguments:".format(len(sys.argv) - 1))
+import sys
 
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+def print_args():
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
+    for i in range(1, num_args + 1):
+        print(f"{i}: {sys.argv[i]}")
+        
+if __name__ == "__main__":
+    print_args()
