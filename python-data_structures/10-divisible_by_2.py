638708b2fa0a568d00864d093ba0aa863433804a
totomus <tom.dibellonio@gmail.com>
2025-01-22 16:24:33 +0100

    returns a list of true and false statement if int %2 = 0

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    result = []
+    for x in my_list:
+        if x % 2 == 0:
+            result.append(True)
+        else:
+            result.append(True)
+    return result
