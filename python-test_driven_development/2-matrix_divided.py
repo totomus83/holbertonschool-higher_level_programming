c464fae6a14a864416d4e93c89446984971b6627
totomus <tom.dibellonio@gmail.com>
2025-01-26 13:56:54 +0100

    wrong error

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -25,7 +25,7 @@ def matrix_divided(matrix, div):
                                 "(list of lists) of integers/floats")
     for row in matrix:
         if len(row) != len(matrix[0]):
-            raise TypeError("Each row must have the same size")
+            raise TypeError("Each row of the matrix must have the same size")
     newmatrix = []
     if div == float("inf"):
         newmatrix = [
