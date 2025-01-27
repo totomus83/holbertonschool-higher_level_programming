0eb3316e83da8bb506889e54d164b77fb08b13f0
totomus83 <tom.dibellonio@gmail.com>
2025-01-27 13:56:24 +0100

    set a class square and a private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+set a class square with a private attribute size
+'''
+
+
+class Square:
+    """class square with private attribute size"""
+    def __init__(self, size):
+        self.__size = size
