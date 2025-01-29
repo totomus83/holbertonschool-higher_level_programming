e207876379b68b832a9dfe4fa2a82afabdaf2a0a
totomus <tom.dibellonio@gmail.com>
2025-01-29 22:14:56 +0100

    test

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -14,10 +14,6 @@ class Square:
     def size(self):
         return self.__size
 
-    @property
-    def position(self):
-        return self.__position
-
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -25,6 +21,10 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
 
     @position.setter
     def position(self, value):
