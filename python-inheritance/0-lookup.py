130f762fd320bef41d8b2c8d78d0017338303fc3
totomus83 <tom.dibellonio@gmail.com>
2025-02-03 12:13:02 +0100

    fixed synthax

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+'''
+function returning the available attributes and method
+'''
+
+
 def lookup(obj):
+    '''
+    return available attribute and mothod of obj
+    '''
     return dir(obj)
