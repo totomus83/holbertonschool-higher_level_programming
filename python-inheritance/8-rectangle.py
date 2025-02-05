afe10befc486d9e3ce5e4ed402f90e2e24f5204c
totomus <tom.dibellonio@gmail.com>
2025-02-05 17:25:03 +0100

    test

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+this file contain a class BaseGeometry and a class rectangle
+inheriting from BaseGeometry
+'''
+
+
+class BaseGeometry:
+    """define BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name=None, value=None):
+        if isinstance(value, bool):
+            raise TypeError(f"{name} must be an integer")
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """define subclass Rectangle who inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
