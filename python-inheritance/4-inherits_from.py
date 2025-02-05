e2baeb139ecfcca5e569b5b9999dd9ca541d9eb2
totomus <tom.dibellonio@gmail.com>
2025-02-05 16:51:04 +0100

    line too long

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 '''
-This function check if obj is an instance of a class that inherited from the specified class
+This function check if obj is an instance of a class that
+inherited from the specified class
 '''
 
 
 def inherits_from(obj, a_class):
     '''
-    Checks if obj an inherited class
+    checks if obj an inherited class
     '''
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
