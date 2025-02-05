3f798754afcd89391069ff114b6b8aa0e9c2c7b6
totomus <tom.dibellonio@gmail.com>
2025-02-05 22:35:41 +0100

    test

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+'''
+this file contain a class BaseGeometry and a class rectangle
+inheriting from BaseGeometry
+'''
+
+
+class BaseGeometry:
+    """define BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if isinstance(value, bool):
+            raise TypeError(f"{name} must be an integer")
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """define subclass Rectangle who inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """define a Square subclass of Rectangle"""
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Rectangle] {self.__size}/{self.__size}"
