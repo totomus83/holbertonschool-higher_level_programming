7366c8acd49f38e00fe96c0de51dfce83e71a36e
totomus <tom.dibellonio@gmail.com>
2025-02-05 16:57:40 +0100

    test

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+this file contain a class BaseGeometry
+'''
+
+
+class BaseGeometry:
+    """define BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
