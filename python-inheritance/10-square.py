e9c24a9d29bb90156897b5989ae55af889ace107
totomus <tom.dibellonio@gmail.com>
2025-02-07 18:43:13 +0100

    test

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,33 +5,7 @@ inheriting from BaseGeometry
 '''
 
 
-class BaseGeometry:
-    """define BaseGeometry"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if isinstance(value, bool):
-            raise TypeError(f"{name} must be an integer")
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """define subclass Rectangle who inherits from BaseGeometry"""
-    def __init__(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__height * self.__width
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
