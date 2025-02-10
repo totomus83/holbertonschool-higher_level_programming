399f7e18d2691ec21d4da972a4c392b13d40f01a
totomus83 <tom.dibellonio@gmail.com>
2025-02-10 15:15:41 +0100

    test

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+this module convert python obj into json str and write it in a file txt
+'''
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''this convert python obj into json str and write it in a file txt'''
+    json_string = json.dumps(my_obj)
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(json_string)
