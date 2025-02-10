8fe55222fc8ffa1ef406079594f19eb5099a1e78
totomus83 <tom.dibellonio@gmail.com>
2025-02-10 16:20:38 +0100

    test

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    my_list = load_from_json_file("add_item.json")
+except Exception:
+    my_list = []
+arguments = sys.argv[1:]
+my_list.extend(arguments)
+save_to_json_file(my_list, "add_item.json")
