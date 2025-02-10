8fc3b5fc6e23444616238aed4dfa1e8b1abecbc5
totomus83 <tom.dibellonio@gmail.com>
2025-02-10 16:23:19 +0100

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+'''
+append a python list to a json file
+'''
+
+
 import json
 import sys
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
