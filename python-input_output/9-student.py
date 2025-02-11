0d9b3a410ba40b56c5e75c57e5de5d3d591b9ac9
totomus <tom.dibellonio@gmail.com>
2025-02-11 14:26:25 +0100

    two blank space

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -2,6 +2,8 @@
 '''
 This module retrieve a dictionary representation of a Student instance
 '''
+
+
 class Student:
     """a class Student that defines a student"""
     def __init__(self, first_name, last_name, age):
