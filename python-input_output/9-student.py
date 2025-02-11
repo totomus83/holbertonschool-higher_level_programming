2a29c42e87e87ac72a1ba91ef51e7e976e08c6fd
totomus <tom.dibellonio@gmail.com>
2025-02-11 14:25:27 +0100

    changed the return

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,15 +3,11 @@
 This module retrieve a dictionary representation of a Student instance
 '''
 class Student:
-    """a class Student that defines a studen"""
+    """a class Student that defines a student"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        return {
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "age": self.age
-        }
+        return self.__dict__
