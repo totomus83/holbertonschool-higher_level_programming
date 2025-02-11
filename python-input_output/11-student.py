5f1643b02da22c2b96db15bf795d3b57dec53bbd
totomus <tom.dibellonio@gmail.com>
2025-02-11 14:59:51 +0100

    test

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+This module retrieve a dictionary representation of a Student instance
+'''
+
+
+class Student:
+    """a class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        filter = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                filter[key] = value
+        return filter
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
+        return self
