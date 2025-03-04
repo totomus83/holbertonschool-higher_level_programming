40ea878a6d4ee446cc32057d0506b6bca52ef8ea
totomus <tom.dibellonio@gmail.com>
2025-03-04 18:11:28 +0100

    test

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- remove all records with a score less than or equal to 5 from the second_table
+DELETE FROM second_table WHERE score <= 5;
---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+script that lists all records from the second_table
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
