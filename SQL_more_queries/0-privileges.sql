b063c659ae1df39743ddcfdb7ce310ca872c070f
Tom <tom.dibellonio@gmail.com>
2025-03-05 17:39:21 +0100

    test

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
---  lists all privileges of the MySQL users 
-SELECT user, host, privilege_type 
-FROM information_schema.user_privileges 
-WHERE user IN ('user_0d_1', 'user_0d_2');
+-- Lists all privileges of user_0d_1 and user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
