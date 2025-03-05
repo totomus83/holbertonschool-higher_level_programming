021785a32ba5b26863f0ee91725adb59a3804934
Tom <tom.dibellonio@gmail.com>
2025-03-05 17:34:48 +0100

    forgot comment

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
-ELECT user, host, privilege_type 
+--  lists all privileges of the MySQL users 
+SELECT user, host, privilege_type 
 FROM information_schema.user_privileges 
 WHERE user IN ('user_0d_1', 'user_0d_2');
