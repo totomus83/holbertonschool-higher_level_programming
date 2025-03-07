4edc8122ed31f8d1aa7ad4e22a04de4a146075ff
totomus <tom.dibellonio@gmail.com>
2025-03-07 16:01:48 +0100

    test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,13 +5,14 @@ This module  lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost", port=3306,
-                        user=username, passwd=password, db=database)
+                         user=username, passwd=password, db=database)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     states = cursor.fetchall()
@@ -21,4 +22,3 @@ if __name__ == "__main__":
 
     cursor.close()
     db.close()
-
